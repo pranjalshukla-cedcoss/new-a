f59a0446ea99f77d42f03817292645d8e0a95a47
cedcoss <pranjalshukla@makewebbetter.com>
2021-01-02 12:09:32 +0530

    second commit

--- index.php
+++ index.php
@@ -15,4 +15,5 @@ define( 'WP_USE_THEMES', true );
 
 /** Loads the WordPress Environment and Template */
 /** This is something changed */
+/** Modify in main branch*/
 require __DIR__ . '/wp-blog-header.php';
